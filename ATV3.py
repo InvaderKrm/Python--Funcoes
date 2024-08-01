9c8e8d16771c0ed6e5b6c0656b44d04f4915bda5
Kramm <134699029+InvaderKrm@users.noreply.github.com>
2024-08-01 10:08:18 -0300

    correcao na media

--- ATV3.py
+++ ATV3.py
@@ -1,12 +1,13 @@
 import math
+
 def valor():
     n = int(input('digite o numero: '))
     return n
+
 def fatorial(n):
-    if n < 0:
-        print("O fatorial não está definido para números negativos")
-    else:
-        print(math.factorial(n))
+    print(math.factorial(n))
+    
 while True:
     n = valor()
-    print('O fatorial do numero',n,'é igual a',fatorial(n))
+    resultado = fatorial(n)
+    print('O fatorial do numero',n,'é igual a',resultado)
--- ATV5.py
+++ ATV5.py
@@ -1,11 +1,22 @@
-def media(numeros):
-    soma=sum(lista)
-    quantidade=len(lista)
-    media = soma/quantidade
+def perguntas():
+    quantidade = int(input('Você deseja calcular a média de quantos números? '))
+    return quantidade
+
+def valores(quantidade):
+    lista = []
+    for i in range(quantidade):
+        numeros = int(input(f" - Digite o {i + 1}º número: "))
+        lista.append(numeros)
+    return lista      
+
+def calculo(lista):
+    media = sum(lista)/len(lista)
     return media
 
-for i in range(3):
-    numero = float(input(f"Digite o {i + 1}º número: "))
-    numeros.append(numero)
+while True:
+    print('Vamos calcular a média de alguns números com Python!')
+    quantidade = perguntas()
+    lista = valores(quantidade)
+    media = calculo(lista)
+    print('A média dos números é igual a',int(media))
     break
-print('A média dos números é igual a:',media(numeros))

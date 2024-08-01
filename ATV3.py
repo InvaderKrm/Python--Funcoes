ff12cd97b3c93013550086cdfc45d84afdbcdbff
Kramm <134699029+InvaderKrm@users.noreply.github.com>
2024-08-01 16:40:31 -0300

    ajustes fatorial

--- ATV3.py
+++ ATV3.py
@@ -1,6 +1,4 @@
 def fatorial(n):
-    if n < 0:
-        return "Não é possível calcular o fatorial de números negativos."
     resultado = 1
     for i in range(1, n + 1):
         resultado *= i
@@ -8,4 +6,7 @@ def fatorial(n):
 
 
 n = int(input('Digite um número: '))
-print(f'O fatorial do número {n} é igual a {fatorial(n)}.')
+if n > 0:
+    print(f'O fatorial do número {n} é igual a {fatorial(n)}.')
+else:
+    print('Não é possível calcular o fatorial de números negativos.')
